3407c7d7c1b58013940e8a8c6257c2bfec46c6e0
Thomas Guignard <tom@timtom.ch>
2021-01-03 21:20:13 -0500

    Update repository with earlier edits

--- functions.php
+++ functions.php
@@ -12,21 +12,41 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 100);
 
 function fb_home_image( $tags ) {
     // Fix the default OpenGraph and Twitter card tags set by Jetpack
-    unset( $tags['og:image'] );
-	unset( $tags['og:image:width'] );
-	unset( $tags['og:image:height'] );
-	unset( $tags['twitter:image'] );
-	unset( $tags['twitter:text:title'] );
+	
+	if($tags['twitter:card']=="summary"){
+	    unset( $tags['og:image'] );
+		unset( $tags['og:image:width'] );
+		unset( $tags['og:image:height'] );
+		unset( $tags['twitter:image'] );
+		unset( $tags['twitter:text:title'] );
 	
  
-	$fb_home_img = "https://pics.timtom.ch/wp-content/uploads/sites/6/2018/02/timtom-flickr-stuttgart01.jpg";
+		$fb_home_img = "https://pics.timtom.ch/wp-content/uploads/sites/6/2018/02/timtom-flickr-stuttgart01.jpg";
 	
-    $tags['og:image'] = esc_url( $fb_home_img );
-    $tags['twitter:image'] = esc_url( $fb_home_img );
-    //$tags['twitter:text:title'] = bloginfo('name');
+	    $tags['og:image'] = esc_url( $fb_home_img );
+	    $tags['twitter:image'] = esc_url( $fb_home_img );
+	    //$tags['twitter:text:title'] = bloginfo('name');
+	}
 	
 	return $tags;
 }
 add_filter( 'jetpack_open_graph_tags', 'fb_home_image' );
 
+/**
+ * Add custom widget area in posts
+ *
+ */
+function timtom_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Custom post widget',
+		'id'            => 'timtom_post_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'timtom_widgets_init' );
 ?>
